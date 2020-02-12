6a6be728dd36021fd8ac174e86a4f6e9ef8207d0
Teddy Kalp <teddy.kalp@gmail.com>
2020-02-12 11:45:24 -0700

    final design changes

--- calc.js
+++ calc.js
@@ -1,43 +1,70 @@
-function updateScreen(x){
-	let screen = document.getElementById('screen')
-	if (screen.value === "0" || screen.value === "ERROR"){
-		screen.value = x;
-	}
-	else if (screen.value.includes("=")){
-		let parts = screen.value.split('=');
-		let answer = parts[parts.length - 1];
-		screen.value = answer + x;
-	}
-	else{
-		screen.value += x;
-	}
-}
+$(document).ready(function(){ 
+    $(".hvr-fade").click(function(){
+    	let val = $(this).text();
+    	if (val === "AC"){
+    		clearScreen();
+    	}
+    	else if (val === "CE"){
+    		backSpace();
+    	}
+    	else if (val === "="){
+    		evalScreen();
+    	}
+    	else if (val === "x"){
+    		updateScreen("*");
+    	}
+    	else{
+    		updateScreen(val);
+    	}
+    });
 
-function clearScreen(){
-	let screen = document.getElementById('screen')
-	screen.value = 0;
-}
-
-function evalScreen(){
-	let screen = document.getElementById('screen')
-	let expression = screen.value
-	try{
-		let answer = eval(expression)
-		screen.value += " = " + answer
+    function updateScreen(x){
+		let screen = document.getElementById('screen')
+		if (screen.value === "0" || screen.value === "ERROR"){
+			screen.value = x;
+		}
+		else if (screen.value.includes("=")){
+			let parts = screen.value.split('=');
+			let answer = parts[parts.length - 1];
+			screen.value = answer + x;
+		}
+		else{
+			screen.value += x;
+		}
 	}
-	catch(error){
-		screen.value = "ERROR"
-	}
-}
 
-function backSpace(){
-	let screen = document.getElementById('screen');
-	let expression = screen.value;
-	if (expression.length === 1 || expression.includes("=") || expression.includes("ERROR")){
+	function clearScreen(){
+		let screen = document.getElementById('screen')
 		screen.value = 0;
 	}
-	else{
-		let newexpression = expression.substring(0,expression.length-1);
-		screen.value = newexpression;
+
+	function evalScreen(){
+		let screen = document.getElementById('screen')
+		let expression = screen.value
+		try{
+			let answer = eval(expression)
+			screen.value += " = " + answer
+		}
+		catch(error){
+			screen.value = "ERROR"
+		}
 	}
-}
+
+	function backSpace(){
+		let screen = document.getElementById('screen');
+		let expression = screen.value;
+		if (expression.length === 1 || expression.includes("=") || expression.includes("ERROR")){
+			screen.value = 0;
+		}
+		else{
+			let newexpression = expression.substring(0,expression.length-1);
+			screen.value = newexpression;
+		}
+	}
+
+});
+
+
+
+
+
